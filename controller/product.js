6ff72f4642cd9bb9732fa33ee4a38f3775a93aa3
patelsaheb1219 <kaumik1219@gmail.com>
2022-01-16 10:53:05 -0500

    Added a new public route to fetch all the products

--- controller/product.js
+++ controller/product.js
@@ -8,9 +8,17 @@ const Product = require("../models/Product");
 // Import ErrorResponse
 const ErrorResponse = require("../utils/errorResponse");
 
+// @desc Get all the products
+// @routes GET /api/v0/product
+// @access Public
+exports.getAllProducts = asyncHandler(async (req, res, next) => {
+  const products = await Product.find();
+  res.status(200).json({ status: true, data: products });
+});
+
 
 // @desc Create a new product
-// @routes POST /api/v0/product
+// @routes POST /api/v0/product/:userId
 // @access Private
 exports.createProduct = asyncHandler(async (req, res, next) => {
   if (req.params.id) {
@@ -25,7 +33,7 @@ exports.createProduct = asyncHandler(async (req, res, next) => {
         price,
         photo,
         user
-      })
+      });
 
       res.status(200).json({ success: true, data: product });
     } else {
