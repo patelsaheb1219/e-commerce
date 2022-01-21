dc9b963d21100a07aa5ca017740a1f39180be5f1
patelsaheb1219 <kaumik1219@gmail.com>
2022-01-21 12:16:55 -0500

    Added routes to get saved item and remove cart item

--- controller/cart.js
+++ controller/cart.js
@@ -9,18 +9,59 @@ const ErrorResponse = require("../utils/errorResponse");
 
 /**
  * @desc Get Added Cart items of a particular user
- * @route GET /api/v0/cart/user
+ * @route GET /api/v0/cart
+ * @access Private
  */
 exports.getCartItems = asyncHandler(async (req, res, next) => {
   if (req.user.id) {
-    const cartItems = await Cart.find({ user: req.user.id, status: "added" }).sort(
-      "-createdAt"
-    );
+    const cartItems = await Cart.find({
+      user: req.user.id,
+      status: "added",
+    }).sort("-createdAt");
+
+    res
+      .status(200)
+      .json({ status: true, count: cartItems.length, data: cartItems });
+  } else {
+    return next(new ErrorResponse(`User not found or not logged In`, 404));
+  }
+});
+
+/**
+ * @desc Get Added Saved Items of a particular user
+ * @route GET /api/v0/cart/saved
+ * @access Private
+ */
+exports.getSavedCartItems = asyncHandler(async (req, res, next) => {
+  if (req.user.id) {
+    const cartItems = await Cart.find({
+      user: req.user.id,
+      status: "saved",
+    }).sort("-createdAt");
+    res
+      .status(200)
+      .json({ status: true, count: cartItems.length, data: cartItems });
+  } else {
+    return next(new ErrorResponse(`User not found or not logged In`, 404));
+  }
+});
+
+/**
+ * @desc Get Added Cart items of a particular user
+ * @route GET /api/v0/cart/user
+ * @access Private
+ */
+exports.getCartItems = asyncHandler(async (req, res, next) => {
+  if (req.user.id) {
+    const cartItems = await Cart.find({
+      user: req.user.id,
+      status: "added",
+    }).sort("-createdAt");
     res.status(200).json({ status: true, data: cartItems });
   } else {
-    return next(new ErrorResponse(`User not found or not logged In`, 404))
+    return next(new ErrorResponse(`User not found or not logged In`, 404));
   }
-})
+});
 
 /**
   @desc Add product to the cart
@@ -31,7 +72,7 @@ exports.addItemToCart = asyncHandler(async (req, res, next) => {
   req.body.user = req.user.id;
   const cart = await Cart.create(req.body);
   res.status(200).json({ status: true, data: cart });
-})
+});
 
 /**
  * @desc Update Cart Item
@@ -41,19 +82,50 @@ exports.addItemToCart = asyncHandler(async (req, res, next) => {
 exports.updateCartItem = asyncHandler(async (req, res, next) => {
   let cartItem = await Cart.findById(req.params.id);
 
+  if (!cartItem) {
+    return next(new ErrorResponse(`Cart Item not found`, 404));
+  }
+
+  if (req.user.id !== cartItem.user.toString()) {
+    return next(
+      new ErrorResponse(`User is not authorize to update this cart Item`, 401)
+    );
+  }
+
+  cartItem = await Cart.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+  });
+
+  res.status(200).json({ status: 200, data: cartItem });
+});
+
+/**
+ * @desc Delete Cart Item
+ * @route DELETE /api/v0/cart/:id
+ * @access Private
+ */
+exports.deleteCartItem = asyncHandler(async (req, res, next) => {
+  let cartItem = await Cart.findById(req.params.id);
 
   if (!cartItem) {
     return next(new ErrorResponse(`Cart Item not found`, 404));
   }
 
   if (req.user.id !== cartItem.user.toString()) {
-    return next(new ErrorResponse(`User is not authorize to update this cart Item`, 401));
+    return next(
+      new ErrorResponse(`User is not authorize to delete this cart Item`, 401)
+    );
   }
 
-  cartItem = await Cart.findByIdAndUpdate(req.params.id, req.body, {
-    new: true,
-    runValidators: true
-  })
-  
-  res.status(200).json({ status: 200, data: cartItem });
+  await Cart.findByIdAndUpdate(
+    req.params.id,
+    { status: "removed" },
+    {
+      new: true,
+      runValidators: true,
+    }
+  );
+
+  res.status(200).json({ status: 200, data: {} });
 });
--- routes/cart.js
+++ routes/cart.js
@@ -5,11 +5,15 @@ const {
   getCartItems,
   updateCartItem,
   addItemToCart,
+  getSavedCartItems,
+  deleteCartItem,
 } = require("../controller/cart");
 const { protect } = require("../middleware/auth");
 
 router.post("/", protect, addItemToCart);
 router.get("/", protect, getCartItems);
+router.get("/saved", protect, getSavedCartItems);
 router.put("/:id", protect, updateCartItem);
+router.delete("/:id", protect, deleteCartItem)
 
 module.exports = router;
